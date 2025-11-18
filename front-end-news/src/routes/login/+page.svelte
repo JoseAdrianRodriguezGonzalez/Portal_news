fe425e75771ad09713eb8285b3b21facdb143c13
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-11-17 19:07:26 -0600

    add the login

--- front-end-news/src/routes/login/+page.svelte
+++ front-end-news/src/routes/login/+page.svelte
@@ -1,5 +1,24 @@
 <script>
-import box from "../../lib/components/box.svelte";
+import Box from "../../lib/components/Box.svelte";
+import Login from "../../layout/Login.svelte";
+import spiderman from "$lib/assets/login/spiderman.png"
 </script>
-<box width="bg-gray">fgdsfsd </box>
-<box width="bg-red"> fdsafsa</box>
+<Box width="bg-[#f2f8ff] grid justify-center items-center">
+  <Login></Login>
+</Box>
+<Box width="bg-[#FFDEDC] place-items-center p-2 hidden md:grid">
+  <div class="relative aspect-square w-full max-w-lg">
+    <div class="absolute inset-0 flex items-center justify-center " >
+      <div class="bg-[#FFB1B1] rounded-full w-6/8 h-6/8 flex items-center justify-center">
+        <div  class="bg-[#FF9D9C] rounded-full w-7/8 h-7/8"></div>
+      </div>
+    </div>
+    <img 
+      src={spiderman}
+      alt="Spiderman"
+      class="absolute top-1/2 left-1/2 -translate-y-5/8 -translate-x-5/8 inset-0 w-3/4 h-3/4 object-contain"
+    />
+  </div>
+  
+  <!--<img src={spiderman}>-->
+</Box>
