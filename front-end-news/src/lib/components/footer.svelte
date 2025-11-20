cda3ffb08186921027c29a0cebd58517767c88c8
Isabel <isabelrocha31415@gmail.com>
2025-11-20 01:03:14 -0600

    cambio del body con su fuente y el fondo correcto, añadi una imagen en la pagina principal

--- front-end-news/src/lib/components/footer.svelte
+++ front-end-news/src/lib/components/footer.svelte
@@ -2,15 +2,14 @@
     import dailyBugle from '$lib/assets/Daily_Bugle.webp';
 </script>
 
-<footer class="bg-red-600 flex flex-col border-y-4 border-red-600">
-  <div class="flex flex-row justify-between items-center py-3">
-    <img src={dailyBugle} alt="Daily Bugle" class="h-15 md:h-12" />
+<footer class="bg-red-600 flex flex-col">
+  <div class="flex items-center justify-between items-center px-2 md:px-4 py-3">
+    <img src={dailyBugle} alt="Daily Bugle" class="h-7 md:h-16" />
 
     <nav class="px-4 py-3">
       <ul class="flex flex-row gap-2 text-white font-medium text-[3.25vw] md:gap-8 md:text-lg">
         <li><a href="/newsletters">Newsletters</a></li>
         <li><a href="/contact">Contact us</a></li>
-        <li><a href="/advertise">Advertise</a></li>
         <li><a href="/about">About us</a></li>
       </ul>
     </nav>
---
+++ front-end-news/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import notaSpider from '$lib/assets/nota.jpg';
+
+</script>
+<p>Holaa soy main</p>
+<div class="basis-2/3 flex justify-end md:justify-center">
+      <img src={notaSpider} alt="News principal SpiderMan" class="h-80 md:h-120"/>
+</div>
+
+  <div class="w-full border-t-2 border-red-600 mt-2"></div>
+<p>hola</p>
