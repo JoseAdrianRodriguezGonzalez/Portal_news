22e2ebd764e9d8c1293bd0dd1d335ad330d1ebe7
Isabel <isabelrocha31415@gmail.com>
2025-11-22 20:36:31 -0600

    cambios en el header y ya estan los dos componentes de las noticias de la pagina principal

---
+++ front-end-news/src/lib/components/news1.svelte
@@ -0,0 +1,55 @@
+<script>
+  export let title;
+  export let imagen;
+  export let link;
+</script>
+
+<a 
+  href={link} 
+  class="
+    block 
+    rounded-[10px]
+    shadow-lg 
+    transition-transform 
+    duration-300 
+    hover:scale-105
+    group
+    w-64
+    bg-white
+    overflow-hidden
+  "
+>
+  <div class="rounded-t-[10px] overflow-hidden h-40">
+    <img 
+      src={imagen} 
+      alt={title} 
+      class="
+        w-full 
+        h-full 
+        object-cover 
+        transition-transform 
+        duration-500 
+        group-hover:scale-105
+      "
+    />
+  </div>
+
+  <div class="
+    bg-red-600 
+    text-white 
+    font-bold 
+    text-base 
+    text-center
+    px-3 
+    py-3 
+    rounded-b-[10px]
+    border-2
+    border-white
+    h-16  
+    flex 
+    items-center 
+    justify-center
+  ">
+    <span class="leading-tight">{title}</span>
+  </div>
+</a>
--- front-end-news/src/routes/+page.svelte
+++ front-end-news/src/routes/+page.svelte
@@ -1,11 +1,31 @@
 <script>
     import notaSpider from '$lib/assets/nota.jpg';
-
+    import News1 from '$lib/components/news1.svelte';
+    import News2 from '$lib/components/news2.svelte';
 </script>
-<p>Holaa soy main</p>
+
 <div class="basis-2/3 flex justify-end md:justify-center">
-      <img src={notaSpider} alt="News principal SpiderMan" class="h-80 md:h-120"/>
+    <img src={notaSpider} alt="News principal SpiderMan" class="h-80 md:h-120"/>
 </div>
 
-  <div class="w-full border-t-2 border-red-600 mt-2"></div>
-<p>hola</p>
+<div class="w-full border-t-2 border-red-600 mt-2"></div>
+
+<section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-6  p-6 
+  max-w-7xl 
+  mx-auto">
+<News1 title="Spider-Man Saves City from Sinister Six Attack" imagen={notaSpider} link="#" />
+<News1 title="Spider-Man Faces Off Against Kingpin in Midtown Brawl" imagen={notaSpider} link="#" />
+<News1 title="Spider-Man vs. Green Goblin" imagen={notaSpider} link="#" />
+<News1 title="Spider-Man Unmasked? " imagen={notaSpider} link="#" />
+</section>
+
+<div class="w-full border-t-2 border-red-600 mt-2"></div>
+
+<section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-6  p-6 
+  max-w-7xl mx-auto">
+
+<News2  section="World news" title="Spider-Man Saves City from Sinister Six Attack" imagen={notaSpider} link="#" content="Estos títulos reflejan el tipo de noticias fantásticas y dramáticas que podría publicar un periódico dentro del mundo de Spider-Man, mezclando villanos clásicos, eventos extraordinarios y la polémica constante que rodea al héroe en la prensa de su univers"/>
+<News2  section="World news" title="Spider-Man Saves City from Sinister Six Attack" imagen={notaSpider} link="#" content="Estos títulos reflejan el tipo de noticias fantásticas y dramáticas que podría publicar un periódico dentro del mundo de Spider-Man, mezclando villanos clásicos, eventos extraordinarios y la polémica constante que rodea al héroe en la prensa de su univers"/>
+<News2  section="World news" title="Spider-Man Saves City from Sinister Six Attack" imagen={notaSpider} link="#" content="Estos títulos reflejan el tipo de noticias fantásticas y dramáticas que podría publicar un periódico dentro del mundo de Spider-Man, mezclando villanos clásicos, eventos extraordinarios y la polémica constante que rodea al héroe en la prensa de su univers"/>
+<News2  section="World news" title="Spider-Man Saves City from Sinister Six Attack" imagen={notaSpider} link="#" content="Estos títulos reflejan el tipo de noticias fantásticas y dramáticas que podría publicar un periódico dentro del mundo de Spider-Man, mezclando villanos clásicos, eventos extraordinarios y la polémica constante que rodea al héroe en la prensa de su univers"/>
+</section>
