4c0821be839acdae796798385805323de12d4f87
Isabel <isabelrocha31415@gmail.com>
2025-11-29 16:01:31 -0600

    correcion footer siempre abajo

--- front-end-news/src/routes/+layout.svelte
+++ front-end-news/src/routes/+layout.svelte
@@ -9,18 +9,18 @@
 
 </script>
 
-
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
 {#if showLayout}
-  <Header />
-  <main>
-    <slot />
-  </main>
-  <Footer />
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    <main class="grow">
+      <slot />
+    </main>
+    <Footer />
+  </div>
 {:else }
   <slot />
 {/if}
-
-<!--  {@render children()} -->
