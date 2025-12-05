36aee0a7d7a61197784a64e39cd49684f7de149d
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-12-04 19:41:39 -0600

    add the url

--- front-end-news/svelte.config.js
+++ front-end-news/svelte.config.js
@@ -1,9 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [preprocess(), vitePreprocess()],
   kit: {
     adapter: adapter(),
     vite: {
