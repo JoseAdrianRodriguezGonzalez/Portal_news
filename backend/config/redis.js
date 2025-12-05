8ccd22d42c803d4dddf245bf8ba5583e0b8a622d
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-12-04 19:17:17 -0600

    add the url

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,7 +1,7 @@
 const { createClient } = require('redis');
 
 const client = createClient({
-    url: process.env.REDIS_URL//`redis://${process.env.REDIS_HOST || 'redis'}:6379`
+    url: "redis://default:OFCIHTDnapnFWRhylBSpiLzqHcwduFMh@centerbeam.proxy.rlwy.net:13511"   //`redis://${process.env.REDIS_HOST || 'redis'}:6379`
 });
 
 client.on('error', (err) => console.log('Error de Cliente Redis', err));
