be6908d49193d15310209bd888e10ab36d64da7d
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-12-04 19:40:56 -0600

    add the url

--- front-end-news/svelte.config.js
+++ front-end-news/svelte.config.js
@@ -1,7 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [preprocess(), vitePreprocess()],
@@ -21,5 +20,9 @@ const config = {
     }
   }
 };
-
 export default config;
+
+
+/** @type {import('@sveltejs/kit').Config} */
+
+
